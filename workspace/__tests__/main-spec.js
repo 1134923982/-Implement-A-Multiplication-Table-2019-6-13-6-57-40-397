49d68bf557de94cd043fc857bb863280d107d463
1134923982 <1134923982@qq.com>
2019-07-10 11:30:24 +0800

    upload file

--- workspace/__tests__/main-spec.js
+++ workspace/__tests__/main-spec.js
@@ -1,27 +1,28 @@
 const main = require('../main');
-const startParameterSmallerThanEndParameter = main.startParameterSmallerThanEndParameter;
+const startNumberSmallerThanEndNumber = main.startNumberSmallerThanEndNumber;
 const isInRangeOf1To1000 = main.isInRangeOf1To1000;
 const createMultiplicationEquation = main.createMultiplicationEquation;
+const outputMultiplicationTable = main.outputMultiplicationTable;
 
 // let str="2*2    \n" +
 //     "2*3    3*3    \n";
 
 it ('should return true when start number is smaller than end number', () => {
-    expect(startParameterSmallerThanEndParameter(2, 3)).toBe(true);
+    expect(startNumberSmallerThanEndNumber(2, 3)).toBe(true);
 });
 it ('should return false when end number is smaller than start number', () => {
-    expect(startParameterSmallerThanEndParameter(4, 3)).toBe(false);
+    expect(startNumberSmallerThanEndNumber(4, 3)).toBe(false);
 });
 it ('should return true when start number is equal end number', () => {
-    expect(startParameterSmallerThanEndParameter(2, 2)).toBe(true);
+    expect(startNumberSmallerThanEndNumber(2, 2)).toBe(true);
 });
 
 it ('should return true when start number and end number between 1 to 1000', () => {
-    expect(isInRangeOf1To1000(10, 200)).toBe(true);
+    expect(isInRangeOf1To1000(1, 1000)).toBe(true);
 });
 
 it ('should return false when start number and end number not in the 1-1000 range', () => {
-    expect(isInRangeOf1To1000(2, 2000)).toBe(false);
+    expect(isInRangeOf1To1000(1, 1001)).toBe(false);
 });
 
 it ('should return false when start number and end number not in the 1-1000 range', () => {
@@ -35,9 +36,20 @@ it ('should return "2*2    \n' +
 });
 
 it ('should return "2*2    \n' +
-    '    2*3    3*3    \n" +\n' +
+    '    2*3    3*3     +\n' +
     '        "2*4    3*4    4*4    \n" when start number and end number between 1 to 1000', () => {
     expect(createMultiplicationEquation(2,4)).toBe("2*2    \n" +
         "2*3    3*3    \n" +
         "2*4    3*4    4*4    \n");
+});
+it ('should return "2*2    \n' +
+    '    2*3    3*3    \n" +\n' +
+    '        "2*4    3*4    4*4    \n" when start number and end number between 1 to 1000', () => {
+    expect(outputMultiplicationTable(2,4)).toBe("2*2    \n" +
+        "2*3    3*3    \n" +
+        "2*4    3*4    4*4    \n");
+});
+
+it ('should return null when start number and end number between 1 to 1000', () => {
+    expect(outputMultiplicationTable(5,4)).toBe(null);
 });
--- workspace/main.js
+++ workspace/main.js
@@ -1,10 +1,10 @@
 
-function startParameterSmallerThanEndParameter(startParam, endParam){
+function startNumberSmallerThanEndNumber(startParam, endParam){
     return startParam<=endParam;
 }
 
 function isInRangeOf1To1000(startParam, endParam){
-    return startParam>0&&startParam<1000&&endParam>0&&endParam<1000;
+    return startParam>0&&startParam<=1000&&endParam>0&&endParam<=1000;
 }
 
 function createMultiplicationEquation(startParam, endParam){
@@ -14,12 +14,18 @@ function createMultiplicationEquation(startParam, endParam){
             multiplicationTable = multiplicationTable+`${j}*${i}    `;
         }
         multiplicationTable=multiplicationTable+'\n';
-        '';
     }
     return multiplicationTable;
 }
 
+function outputMultiplicationTable(startNubmer, endNumber){
+    if(startNumberSmallerThanEndNumber(startNubmer, endNumber)&&isInRangeOf1To1000(startNubmer, endNumber)){
+        return createMultiplicationEquation(startNubmer, endNumber);
+    }
+    return null;
+}
 
-exports.startParameterSmallerThanEndParameter = startParameterSmallerThanEndParameter;
+exports.startNumberSmallerThanEndNumber = startNumberSmallerThanEndNumber;
 exports.isInRangeOf1To1000 = isInRangeOf1To1000;
 exports.createMultiplicationEquation = createMultiplicationEquation;
+exports.outputMultiplicationTable = outputMultiplicationTable;
